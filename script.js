e20cf0d04a02da1c79f8f0199aaee4ee143a5e4d
lin., frank <jacob127871@student-schoolsofwestfield.org>
2025-04-16 11:50:02 -0400

    4/16 blockrow+col

--- script.js
+++ script.js
@@ -133,6 +133,7 @@ function pcBlockRow(row){
         console.log("pcBlockRow - BOX: " + box);
         if (checkPlayer(box) == "-") return box;
     }
+    // debugger;
 }
 
 function pcBlockCol(col){
@@ -149,18 +150,23 @@ function pcBlockCol(col){
  */
 function pcCheckRow(row){
     let x_count = 0;
+    let count = 0;
     for (let box = row; box < row + 3; box++){ 
         // if box contains x add 1 to x_count
         if (movesTaken.includes(box)){
-            x_count++;
+            if (checkPlayer(box) == "X") x_count++;
+            count++;
             console.log(" - x_count: " + x_count + " | index: " + movesTaken.indexOf(box));
         }
         // else {
         //     console.log(box + " not in " + movesTaken.toString());
         // }
     }
-    if (x_count < 2) return false;
-    else return true;
+    if (x_count == 2 && count != 3){ 
+        console.error("checkrow true");
+        debugger;
+        return true; }
+    else return false;
 }
 
 /**
@@ -170,12 +176,18 @@ function pcCheckRow(row){
  */
 function pcCheckCol(col){
     let x_count = 0;
+    let count = 0;
     for (let box = col; box < 10; box += 3){ 
         // if box contains x add 1 to x_count
-        if (movesTaken.includes(box)) x_count++;
+        if (movesTaken.includes(box)) {
+            if (checkPlayer(box) == "X") x_count++;
+            count++;
+        }
     }
-    if (x_count < 2) return false;
-    else return true;
+    if (x_count == 2 && count != 3) {
+        console.error("checkcol true");
+        return true;}
+    else return false;
 }
 
 /**
